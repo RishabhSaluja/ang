33d9b9ff846902527077ba2fa8da0fb8263adaef
RishabhSaluja <rishabh.saluja20@gmail.com>
2017-09-28 18:13:12 +0530

    added edit feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms'
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -10,50 +11,65 @@ export class AppComponent implements OnInit {
   title = 'app';
   form1: FormGroup;
   addclicked = false;
-  
+  tempId = '';
+  tempindex;
+  editclicked = false;
+
   ngOnInit() {
     this.form1 = new FormGroup({
       'id': new FormArray([]),
       'name': new FormArray([]),
       'weight': new FormArray([]),
       'quantity': new FormArray([])
-
+      
       // 'id': new FormArray([new FormControl('1'), new FormControl('2')]),
       // 'name': new FormArray([new FormControl('Beans'), new FormControl('Soup')]),
       // 'weight': new FormArray([new FormControl('100'), new FormControl('125')]),
       // 'quantity': new FormArray([new FormControl('60'), new FormControl('20')])
-      
+
     });
   }
   addRow(){
     this.addclicked = true;
-  }
-  getvisible(){
-    return this.addclicked === false ? 'inline-block' : 'none';
+    this.tempId = '';
   }
   hitcancel(){
     this.addclicked = false;
+    this.tempId = '';
   }
-
-  hitadd(){
+  hitadd(name, weight, quantity){
+    this.tempId = (this.ids.value.length + 1).toString(10);
+    
+    this.ids.push(new FormControl(this.tempId));
+    this.names.push(new FormControl(name));
+    this.weights.push(new FormControl(weight));
+    this.quantities.push(new FormControl(quantity));
+    
+    this.tempId = '';
     this.addclicked = false;
-    const control = new FormControl(null);
-    (<FormArray>this.form1.get('id')).push(control);
+
   }
 
-  editRow(){    
+  editRow(index: number){
+    this.editclicked = true;
+    this.tempindex = index;
+    
+    //this.ids.at(index).patchValue(null);
   }
 
-  delRow(id, name, weight, quantity:FormControl){
-    let index1 = this.ids.controls.indexOf(id);
-    let index2 = this.names.controls.indexOf(name);
-    let index3 = this.weights.controls.indexOf(weight);
-    let index4 = this.quantities.controls.indexOf(quantity);
+  delRow(index: number){
+    this.addclicked = false;
+    
+    this.ids.removeAt(index);
+    this.names.removeAt(index);
+    this.weights.removeAt(index);
+    this.quantities.removeAt(index);
 
-    this.ids.removeAt(index1);
-    this.names.removeAt(index2);
-    this.weights.removeAt(index3);
-    this.weights.removeAt(index4);
+    let i = 0;
+    this.ids.controls.forEach(element => {
+      i++;
+      element.setValue(i.toString(10))
+    });
   }
   get ids(){
     return this.form1.get('id') as FormArray;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
-
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
