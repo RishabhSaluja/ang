c0b23bb87db854ec54196b0d7d5e0a83c01d244a
RishabhSaluja <rishabh.saluja20@gmail.com>
2017-09-29 21:57:16 +0530

    Some fixes!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,73 +9,102 @@ import { element } from 'protractor';
 })
 export class AppComponent implements OnInit {
   title = 'app';
+
   form1: FormGroup;
+
+  //State of button clicked
   addclicked = false;
+  editclicked = false;
+
+  //temp variable to store previous values
   tempId = '';
   tempName = '';
   tempWeight = '';
   tempQuantity = '';
+
+  //temp index of selected row
   tempindex;
-  editclicked = false;
 
   ngOnInit() {
     this.form1 = new FormGroup({
-      // 'id': new FormArray([]),
-      // 'name': new FormArray([]),
-      // 'weight': new FormArray([]),
-      // 'quantity': new FormArray([])
+      'id': new FormArray([]),
+      'name': new FormArray([], Validators.required),
+      'weight': new FormArray([], Validators.required),
+      'quantity': new FormArray([], Validators.required)
 
-
-      'id': new FormArray([new FormControl('1'), new FormControl('2')]),
-      'name': new FormArray([new FormControl('Beans'), new FormControl('Soup')]),
-      'weight': new FormArray([new FormControl('100'), new FormControl('125')]),
-      'quantity': new FormArray([new FormControl('60'), new FormControl('20')])
-
+      // 'id': new FormArray([new FormControl('1'), new FormControl('2')]),
+      // 'name': new FormArray([new FormControl('Beans'), new FormControl('Soup')]),
+      // 'weight': new FormArray([new FormControl('100'), new FormControl('125')]),
+      // 'quantity': new FormArray([new FormControl('60'), new FormControl('20')])]
     });
   }
-  addRow(){
+
+  //
+  //Button to initiate add functionality 
+  //
+  addRow() {
     this.addclicked = true;
     this.tempId = '';
   }
-  hitcancel(){
+
+  //
+  //Button to cancel the last event
+  //
+  hitcancel() {
     this.addclicked = false;
     this.tempId = '';
   }
-  hitadd(name, weight, quantity){
-    this.tempId = (this.ids.value.length + 1).toString(10);
 
-    this.ids.push(new FormControl(this.tempId));
-    this.names.push(new FormControl(name));
-    this.weights.push(new FormControl(weight));
-    this.quantities.push(new FormControl(quantity));
+  //
+  //Add new value to the row / FormArray
+  //  
+  hitadd(name, weight, quantity) {
 
-    this.tempId = '';
-    this.addclicked = false;
+    //Check input fields are empty or not
+    if (name !== '' && weight !== '' && quantity !== '') {
+      this.tempId = (this.ids.value.length + 1).toString(10);
 
-    console.log(this.form1);
+      //To add input fields values to FormArray
+      this.ids.push(new FormControl(this.tempId));
+      this.names.push(new FormControl(name));
+      this.weights.push(new FormControl(weight));
+      this.quantities.push(new FormControl(quantity));
+
+      this.tempId = '';
+      this.addclicked = false;
+    }
   }
 
+  //
+  //method to temporary save the row values
+  // &
+  //fetch the current row index to edit
+  //
   editRow(index: number) {
     this.tempName = this.names.value[index];
     this.tempWeight = this.weights.value[index];
     this.tempQuantity = this.quantities.value[index];
     this.editclicked = true;
     this.tempindex = index;
-
-    //this.ids.at(index).patchValue(null);
   }
 
-  saveRow(index: number, name, weight, quantity){
+  //
+  //method to save the edited row elements
+  //
+  saveRow(index: number, name, weight, quantity) {
 
-        this.names.at(index).setValue(name);
-        this.weights.at(index).setValue(weight);
-        this.quantities.at(index).setValue(quantity);
+    this.names.at(index).setValue(name);
+    this.weights.at(index).setValue(weight);
+    this.quantities.at(index).setValue(quantity);
 
-        this.editclicked = false;
-        this.tempindex = undefined;
-      }
+    this.editclicked = false;
+    this.tempindex = undefined;
+  }
 
-  delRow(index: number){
+  //
+  //method to delete a row from the table
+  //
+  delRow(index: number) {
     this.addclicked = false;
 
     this.ids.removeAt(index);
@@ -83,22 +112,31 @@ export class AppComponent implements OnInit {
     this.weights.removeAt(index);
     this.quantities.removeAt(index);
 
+    //
+    //Track the product id whenever a product
+    //gets deleted
+    //
     let i = 0;
     this.ids.controls.forEach(element => {
       i++;
       element.setValue(i.toString(10))
     });
   }
-  get ids(){
+
+  //
+  //get methods to return the form controls
+  //
+  get ids() {
     return this.form1.get('id') as FormArray;
   }
-  get names(){
+  get names() {
     return this.form1.get('name') as FormArray;
   }
-  get weights(){
+  get weights() {
     return this.form1.get('weight') as FormArray;
   }
-  get quantities(){
+  get quantities() {
     return this.form1.get('quantity') as FormArray;
   }
+
 }
